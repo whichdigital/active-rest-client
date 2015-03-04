f0bf2bd797e284fdd9147def836eb04c719427e3
Tiago Teixeira <tiagocmtex@gmail.com>
2015-03-04 22:06:09 +0200

    rename method

--- lib/active_rest_client/request.rb
+++ lib/active_rest_client/request.rb
@@ -130,7 +130,7 @@ module ActiveRestClient
           @object.class.send(:_filter_request, :before, @method[:name], self)
         end
         append_get_parameters
-        prepare_request_body_type
+        prepare_request_body
         self.original_url = self.url
         cached = original_object_class.read_cached_response(self)
         if cached
@@ -206,7 +206,7 @@ module ActiveRestClient
       end
     end
 
-    def prepare_request_body_type(params = nil)
+    def prepare_request_body(params = nil)
       if request_body_type == :form_encoded
         @body ||= (params || @post_params || {}).map {|k,v| "#{k}=#{CGI.escape(v.to_s)}"}.sort * "&"
         headers["Content-Type"] = "application/x-www-form-urlencoded"
