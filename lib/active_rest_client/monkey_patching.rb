ec28d3691644c64a73fd5940ff1d96881fa1fa23
Andy Jeffries <andy@andyjeffries.co.uk>
2015-09-03 13:06:50 +0100

    Trying to fix compatibility with older versions of Faraday

--- lib/active_rest_client/monkey_patching.rb
+++ lib/active_rest_client/monkey_patching.rb
@@ -1,3 +1,7 @@
-class Faraday::Env
-  alias_method :headers, :response_headers
+require 'faraday'
+
+if defined?("Faraday::Env")
+  class Faraday::Env
+    alias_method :headers, :response_headers
+  end
 end
